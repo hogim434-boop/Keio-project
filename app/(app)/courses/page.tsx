f2021546a8c4c5161ba54bb98f61af5813bb4fd1
KIMHO128 <hogim434@gmail.com>
2026-05-04 15:15:32 +0900

    🔥 chore: 강의/검색/community 라우트·컴포넌트·더미·5축 평점 타입 일괄 폐기
    자유 게시판 피벗으로 강의 도메인 자산 제거.
    - app/(app)/courses, search, community 라우트 폴더 통째 삭제
    - course-card, rating-bar-chart, star-rating-input 컴포넌트 삭제
    - lib/dummy-data, lib/dummy-community 더미 데이터 삭제
    - types/rating.ts 5축 평점 타입 삭제 (게시판 미사용)

--- app/(app)/courses/page.tsx
+++
@@ -1,6 +0,0 @@
-import { DUMMY_COURSES } from '@/lib/dummy-data'
-import { CoursesClient } from './_components/courses-client'
-
-export default function CoursesPage() {
-  return <CoursesClient courses={DUMMY_COURSES} />
-}
--- app/(app)/search/page.tsx
+++
@@ -1,6 +0,0 @@
-import { DUMMY_COURSES } from '@/lib/dummy-data'
-import { SearchClient } from './_components/search-client'
-
-export default function SearchPage() {
-  return <SearchClient courses={DUMMY_COURSES} />
-}
